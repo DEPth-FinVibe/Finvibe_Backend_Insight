cdc65a0a3bfc7a582aa548628043a158889962b9
taek <wontaek88@gmail.com>
2026-01-24 19:18:03 +0900

    feat: 초기 세팅

---
+++ src/test/java/finvibe/insight/InsightApplicationTests.java
@@ -0,0 +1,13 @@
+package finvibe.insight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
