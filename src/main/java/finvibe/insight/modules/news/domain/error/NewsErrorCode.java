b72846bab5f8cf84a4550303ca5cc3b81b0c0cc8
taek <wontaek88@gmail.com>
2026-01-25 23:02:27 +0900

    feat: 도메인 및 뉴스 에러 코드, 예외 처리 공통 모듈 구현 및 뉴스 서비스 예외 적용

---
+++ src/main/java/finvibe/insight/modules/news/domain/error/NewsErrorCode.java
@@ -0,0 +1,16 @@
+package finvibe.insight.modules.news.domain.error;
+
+import finvibe.insight.shared.error.DomainErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum NewsErrorCode implements DomainErrorCode {
+
+    NEWS_NOT_FOUND("NEWS_NOT_FOUND", "존재하지 않는 뉴스입니다."),
+    COMMENT_NOT_FOUND("COMMENT_NOT_FOUND", "존재하지 않는 댓글입니다.");
+
+    private final String code;
+    private final String message;
+}
