7f84d7a78b00b124c5bcc07bcbd7dd7e7ac01ed9
taek <wontaek88@gmail.com>
2026-01-26 15:02:18 +0900

    feat: 뉴스 목록 정렬 기능 추가(최신순, 인기순)

--- src/main/java/finvibe/insight/modules/news/application/port/in/NewsQueryUseCase.java
+++ src/main/java/finvibe/insight/modules/news/application/port/in/NewsQueryUseCase.java
@@ -1,12 +1,12 @@
 package finvibe.insight.modules.news.application.port.in;
 
-import finvibe.insight.modules.news.dto.DiscussionDto;
 import finvibe.insight.modules.news.dto.NewsDto;
+import finvibe.insight.modules.news.dto.NewsSortType;
 
 import java.util.List;
 
 public interface NewsQueryUseCase {
-    List<NewsDto.Response> findAllNewsSummary();
+    List<NewsDto.Response> findAllNewsSummary(NewsSortType sortType);
 
     NewsDto.DetailResponse findNewsById(Long id);
 }
