338d4b2f671a57742a56290136ec6d478cc55f0d
taek <wontaek88@gmail.com>
2026-02-02 23:15:26 +0900

    feat: 테스트 코드 외부 api 목킹 처리

--- src/test/java/finvibe/insight/InsightApplicationTests.java
+++ src/test/java/finvibe/insight/InsightApplicationTests.java
@@ -1,10 +1,28 @@
 package finvibe.insight;
 
+import dev.langchain4j.model.chat.ChatModel;
+import finvibe.insight.modules.news.infra.client.HttpDiscussionClient;
+import finvibe.insight.modules.news.application.port.out.NewsCrawler;
+import finvibe.insight.shared.dto.DiscussionEvent;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InsightApplicationTests {
+
+	@MockBean
+	private ChatModel chatModel;
+
+	@MockBean
+	private HttpDiscussionClient httpDiscussionClient;
+
+	@MockBean
+	private KafkaTemplate<String, DiscussionEvent> kafkaTemplate;
+
+	@MockBean
+	private NewsCrawler newsCrawler;
 
 	@Test
 	void contextLoads() {
